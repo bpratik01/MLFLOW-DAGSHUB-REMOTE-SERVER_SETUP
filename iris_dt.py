ab018433d4d358e6b50719333f92eb9c38cadbd3
Pratik <pratikrbokade05@gmailcom>
2025-01-23 11:12:43 +0530

    added and initaited dagshub with mlflow=True

--- iris_dt.py
+++ iris_dt.py
@@ -6,6 +6,11 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 
+import dagshub
+dagshub.init(repo_owner='bpratik01', repo_name='IRIS_REMOTE_SERVER', mlflow=True)
+
+
+mlflow.set_tracking_uri('https://dagshub.com/bpratik01/IRIS_REMOTE_SERVER.mlflow')
 data = load_iris()
 
 X, y = data.data, data.target
@@ -27,5 +32,20 @@ with mlflow.start_run(run_name = 'detree_iris'):
 
     mlflow.log_metric('accuracy', accuracy)
     mlflow.log_param('max_depth', max_depth)
+
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,7))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=data.target_names, yticklabels=data.target_names, cbar=False)
+    plt.xlabel('Predicted')
+    plt.ylabel('Truth')
+
+    plt.savefig('confusion_matrix.png')
+    mlflow.log_artifact('confusion_matrix.png', 'images')
+
+    mlflow.log_artifact('iris_dt.py')
+
+    mlflow.sklearn.log_model(model, 'model')
+
+    print(f'Accuracy: {accuracy}')
 
     
